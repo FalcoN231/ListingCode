c4327b68fedbea3e1aefe89f70bda3be89bbf05d
bodya <solo.logger@gmail.com>
2024-12-17 22:44:37 +0300

    realese

--- ListingCode/Data/DocGenerator.cs
+++ ListingCode/Data/DocGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
+using Xceed.Words.NET;
 
 namespace ListingCode.Data;
 
@@ -11,17 +12,24 @@ public class DocGenerator
       {
             try
             {
-                  using (StreamWriter writer = new(filePath))
+                  using (DocX document = DocX.Create(filePath))
                   {
                         foreach (var file in files)
                         {
-                              writer.WriteLine($"Файл {Path.GetFileName(file)}");
+                              document.InsertParagraph(Path.GetFileName(file))
+                                    .Font("Times New Roman")
+                                    .Bold()
+                                    .FontSize(14)
+                                    .SpacingAfter(12);
 
                               string fileContent = File.ReadAllText(file);
-                              writer.WriteLine(fileContent);
-
-                              writer.WriteLine();
+                              document.InsertParagraph(fileContent)
+                                    .Font("Times New Roman")
+                                    .FontSize(12)
+                                    .SpacingAfter(6);
                         }
+
+                        document.Save();
                   }
 
                   MessageBox.Show("Listing saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
